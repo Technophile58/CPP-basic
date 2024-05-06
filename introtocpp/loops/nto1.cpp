d6720f2bf5c459114b5bd80c4c7121a24ec70141
TusharPant <133578207+Technophile58@users.noreply.github.com>
2024-05-06 11:41:26 +0530

    Add files via upload

---
+++ introtocpp/loops/nto1.cpp
@@ -0,0 +1,16 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+int main() {
+    // Write C++ code here
+    int n;
+    cout<<"Enter the value of n :";
+    cin>>n;
+
+for(int i=n;i>=1;i--){
+    cout<<i << " ";
+
+}
+ 
+    return 0;
+}
