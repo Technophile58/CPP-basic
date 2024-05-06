c2ba5e17b711dbd6d1e24e8a4db30b96d5a9d07a
TusharPant <133578207+Technophile58@users.noreply.github.com>
2024-05-06 13:56:19 +0530

    Update 1tonEven.cpp

--- introtocpp/loops/1tonEven.cpp
+++ introtocpp/loops/1tonEven.cpp
@@ -6,7 +6,7 @@ int main() {
     int n,count=0;
     cout<<"Enter the value of n :";
     cin>>n;
-    for(int i=1;i<=n;i++){
+    for(int i=2;i<=n;i++){
         if(i%2==0){
             cout<<i <<" ";
         }
